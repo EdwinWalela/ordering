bd57d5df0e8c9c870bd5eda78483c81452a66a3c
edwinwalela <edwinwalela@gmail.com>
2022-04-30 12:24:05 +0300

    update db query

--- db.sql
+++ db.sql
@@ -1,14 +1,14 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE customers(
-  id BIGSERIAL PRIMARY KEY
-  name VARCHAR(50) NOT NULL
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
   code VARCHAR(50) NOT NULL DEFAULT uuid_generate_v4 ()
 );
 
 CREATE TABLE orders(
-  id BIGSERIAL PRIMARY KEY
-  item VARCHAR(100) NOT NULL
-  amount INTEGER
-  user_id BIGINT REFERENCES "customers" ("id")
-  created_at TIMESTAMPZ DEFAULT (now() at time zone 'utc')
+  id BIGSERIAL PRIMARY KEY,
+  item VARCHAR(100) NOT NULL,
+  amount INTEGER,
+  customer_id BIGINT REFERENCES "customers" ("id"),
+  created_at TIMESTAMPTZ DEFAULT (now() at time zone 'utc')
 )
